d21b53703145a9437b0c0ed0a3feaa13d2a6b1ce
SaburovAidar <SaburovAidar1312@gmail.com>
2026-06-14 14:15:35 +0300

    Update BOT_TOKEN and add ADMIN_ID
    Replaced environment variable for BOT_TOKEN with a hardcoded token and added ADMIN_ID.

--- config.py
+++ config.py
@@ -1,14 +1,2 @@
-import os
-
-BOT_TOKEN       = os.environ.get("BOT_TOKEN", "ВАШ_ТОКЕН")
-APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbye1M-P0gLaUdqkhv77YaEvJhN3EzqB9GHQTyOLNyhl_owL5ivC0APr7WEdIfn0mdLM/exec"
-
-CHARACTER_NAME = "Олег Сергеевич\n📱 @OlegSergeevichGibdd"
-CHARACTER_DESCRIPTION = (
-    "🔰 Помощь с авто-документами\n"
-    "🚗 Водительские удостоверения\n"
-    "📑 СТС • ПТС • Документы автошколы\n"
-    "🏥 Медицинские справки\n"
-    "⚡ Оперативно и надёжно"
-)
-CHARACTER_IMAGE_URL   = "https://i.postimg.cc/QM9p9mhJ/111.jpg"
+TOKEN = "8823346218:AAEh_wUjlCPmUYHCKyWvxqEm2sklHY5dp6Y"
+ADMIN_ID = 123456789
